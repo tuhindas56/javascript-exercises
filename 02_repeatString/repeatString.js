8345919c71d6fafa8269bf70d601115acc392a04
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-02-21 12:26:49 +0530

    Finish exercise 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,6 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  return string.repeat(num);
 };
-
+// repeatString("hey", 3);
 // Do not edit below this line
 module.exports = repeatString;

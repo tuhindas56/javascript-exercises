57ec4816ea0dd140ce2a15956c890727bbcdd5d0
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-02-22 18:25:51 +0530

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (firstNumber, lastNumber) {
+  if (typeof firstNumber === "number" && typeof lastNumber === "number") {
+    if (firstNumber > 0 && lastNumber > 0) {
+      if (firstNumber < lastNumber) {
+        return (
+          ((lastNumber - firstNumber + 1) * (firstNumber + lastNumber)) / 2
+        );
+      } else {
+        return (
+          ((firstNumber - lastNumber + 1) * (lastNumber + firstNumber)) / 2
+        );
+      }
+    } else {
+      return "ERROR";
+    }
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+

6be333699c81237fa60485a6119b48eccda12851
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-02-22 22:36:04 +0530

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numberArray) {
+  return numberArray.reduce((sum, value) => sum + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numberArray) {
+  return numberArray.reduce((multiply, value) => multiply * value, 1);
 };
 
-const power = function() {
-	
+const power = function (number, raisedTo) {
+  return number ** raisedTo;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number == 0) {
+    return 1;
+  }
+  let factorial = 1;
+  for (let i = number; i > 0; i--) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
@@ -29,5 +36,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
